4413064544de1e675a6eb48f317db039825bb806
Carlos Rojas <j.carlosrojas@outlook.com>
2023-04-09 01:16:12 -0500

    función corregida listaTiempoPara

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -24,9 +24,39 @@ function actualizarBarraProgreso() {
 }
 
 
+//función para el tiempo para terminar la ingenieria y el TSU
+let tiempoParaIngenieria = 0;
+let tiempoParaTsu = 0;
+
+let listaTiempoPara = document.getElementById("tiempo_Para");
+function tiempo_Para() {
+    let tiempoParaIngenieria = (( FechaFinalIngenieria.getTime() - new Date().getTime()  ) / (1000 * 60 * 60));
+    let tiempoParaTsu = (( FechaFinalTsu.getTime() - new Date().getTime()  ) / (1000 * 60 * 60));
+
+    //borra los elementos de la lista para que no se acumulen
+    listaTiempoPara.innerHTML = "";
+
+    //añade los elementos a la lista "tiempoPara"
+    let liTiempoIngenieria = document.createElement("li");
+    liTiempoIngenieria.textContent = `Ingeniería: ${tiempoParaIngenieria.toFixed(4)} horas`;
+    listaTiempoPara.appendChild(liTiempoIngenieria);
+
+    let liTiempoTsu = document.createElement("li");
+    liTiempoTsu.textContent = `TSU: ${tiempoParaTsu.toFixed(4)} horas`;
+    listaTiempoPara.appendChild(liTiempoTsu);
+
+}
+
+
+
+
+
+
 //invocador de la función al cargar la página y cada 5 segundos después
 document.addEventListener("DOMContentLoaded", function () {
     actualizarBarraProgreso();
-    setInterval(actualizarBarraProgreso, 5000);
+    tiempo_Para();
+    setInterval(actualizarBarraProgreso, 25000);
+    setInterval(tiempo_Para, 5000);
 });
 
